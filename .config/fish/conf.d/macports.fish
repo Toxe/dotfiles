77ed8f9f3970ac9dbe6517bc34cf15be1087df25
Toxe <toxe@toxe.net>
2022-06-27 20:29:46 +0200

    fish: add MacPorts and VSCode configs

---
+++ .config/fish/conf.d/macports.fish
@@ -0,0 +1,3 @@
+if test -d /opt/local/bin
+    set -gx PATH /opt/local/bin /opt/local/sbin $PATH
+end
---
+++ .config/fish/conf.d/vscode.fish
@@ -0,0 +1,3 @@
+if test -f "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+    set -gx PATH $PATH "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+end
